cbbfbc287617a3c6e8497f682ddb1281d5a4d103
Sam <samir.9898580540@gmail.com>
2018-04-18 23:46:20 +0530

    Java, xml and data files

---
+++ HybridFW/src/utility/Constant.java
@@ -0,0 +1,27 @@
+package utility;
+
+public class Constant {
+	    public static final String URL = "http://store.demoqa.com/";
+	    	//	 "http://www.onlinestore.toolsqa.com";
+	    public static final String Username = "samir.9898580540@gmail.com";
+	    public static final String Password ="rrr@2012";
+		public static final String Path_TestData = "E://eclipse-oxygen-workspace//ToolsQA//HybridFW//src//testData//";
+		public static final String File_TestData = "TestData.xlsx";
+		
+		//Test Data Sheet Columns
+		public static final int Col_TestCaseName = 0;	
+		public static final int Col_UserName =1 ;
+		public static final int Col_Password = 2;
+		public static final int Col_Browser = 3;
+		public static final int Col_ProductType = 4;
+		public static final int Col_ProductNumber = 5;
+		public static final int Col_FirstName = 6;
+		public static final int Col_LastName = 7;
+		public static final int Col_Address = 8;
+		public static final int Col_City = 9;
+		public static final int Col_Country = 10;
+		public static final int Col_Phone = 11;
+		public static final int Col_Email = 12;
+		public static final int Col_Result = 13;
+		public static final String Path_ScreenShot = "E://eclipse-oxygen-workspace//ToolsQA//HybridFW//src//Screenshots//";
+	}
